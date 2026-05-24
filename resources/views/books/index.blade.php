dfc8e49fc651c8aa53938d3eb1aa4325ec39280d
Michel du Croix <michel.ducroix@ultimaker.com>
2026-05-24 17:50:04 +0200

    Create books index view - create new layout - create new books index view - add overview of books - add search form

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+@section('content')
+    <h1 class="mb-10 text-2xl">Books</h1>
+
+    <form method="GET" action="{{ route('books.index') }}" class="mb-6">
+        <div class="flex items-center space-x-2">
+            <input type="text" name="title" placeholder="Search by title" value="{{ request('title') }}" class="input">
+            <button type="submit" class="btn">Search</button>
+            <a href="{{ route('books.index') }}" class="btn">Reset</a>
+        </div>
+
+
+    </form>
+
+    <ul>
+        @forelse ($books as $book)
+            <li class="mb-4">
+                <div class="book-item">
+                    <div
+                    class="flex flex-wrap items-center justify-between">
+                    <div class="w-full flex-grow sm:w-auto">
+                        <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+                        <span class="book-author">by {{ $book->author }}</span>
+                    </div>
+                    <div>
+                        <div class="book-rating">
+                            {{ number_format($book->reviews_avg_rating, 1) }}
+                        </div>
+                        <div class="book-review-count">
+                            out of {{ $book->reviews_count }} {{ Str::plural('review', $book->review_count) }}
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </li>
+        @empty
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text">No books found</p>
+                    <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
+                </div>
+            </li>
+        @endempty
+    </ul>
+
+
+
+@endsection
