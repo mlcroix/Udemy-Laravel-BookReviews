30a8fb9d257bd0e2a38984d18cd74d341e288f63
Michel du Croix <michel.ducroix@ultimaker.com>
2026-05-24 16:52:24 +0200

    Add more scopes to book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -18,5 +19,32 @@ class Book extends Model
 
     public function scopeTitle(Builder $query, string $title): Builder{
         return $query->where('title', 'like', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder{
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder{
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder{
+        return $query->has('reviews', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from, $to): Builder {
+        if ($from && !$to) {
+            return $query->where('created_at', '>=', $from);
+        } else if (!$from && $to) {
+            return $query->where('created_at', '<=', $to);
+        } else if ($from && $to) {
+            return $query->whereBetween('created_at', [$from, $to]);
+        }
+        return $query;
     }
 }
